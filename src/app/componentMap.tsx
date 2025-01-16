3416096c0aef4ded6252432b3d4570779f669a09
Dia Loghmari <dia.loghmari@lcieducation.com>
2025-01-15 20:26:47 -0500

    feat: force the accordion component to be RSC

---
+++ src/app/componentMap.tsx
@@ -0,0 +1,10 @@
+import dynamic from "next/dynamic";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const componentMap: Record<string, React.ComponentType<any>> = {
+  Header: dynamic(() => import("./components/Header"), { ssr: true }),
+  Carousel: dynamic(() => import("./components/Carousel"), { ssr: true }),
+  Accordion: dynamic(() => import("./components/AccordionDemo"), {
+    ssr: true,
+  }),
+};
--- src/app/components/AccordionDemo.tsx
+++ src/app/components/AccordionDemo.tsx
@@ -1,38 +1,54 @@
-import React from "react";
 import * as Accordion from "@radix-ui/react-accordion";
-import classNames from "classnames";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
+import classNames from "classnames";
+import React from "react";
 import styles from "./accordion.module.css";
 
-const AccordionDemo: React.FC = () => (
-  <Accordion.Root
-    className={styles.Root}
-    type="single"
-    defaultValue="item-1"
-    collapsible
-  >
-    <Accordion.Item className={styles.Item} value="item-1">
-      <AccordionTrigger>Is it accessible?</AccordionTrigger>
-      <AccordionContent>
-        Yes. It adheres to the WAI-ARIA design pattern.
-      </AccordionContent>
-    </Accordion.Item>
+const AccordionDemo: React.FC = async () => {
+  const response = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+  );
+  const data = await response.json();
+  const latitude = data.latitude;
 
-    <Accordion.Item className={styles.Item} value="item-2">
-      <AccordionTrigger>Is it unstyled?</AccordionTrigger>
-      <AccordionContent>
-        Yes. It&apos;s unstyled by default, giving you freedom over the look and feel.
-      </AccordionContent>
-    </Accordion.Item>
+  return (
+    <Accordion.Root
+      className={styles.Root}
+      type="single"
+      defaultValue="item-1"
+      collapsible
+    >
+      <Accordion.Item className={styles.Item} value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </Accordion.Item>
 
-    <Accordion.Item className={styles.Item} value="item-3">
-      <AccordionTrigger>Can it be animated?</AccordionTrigger>
-      <AccordionContent>
-        Yes! You can animate the Accordion with CSS or JavaScript.
-      </AccordionContent>
-    </Accordion.Item>
-  </Accordion.Root>
-);
+      <Accordion.Item className={styles.Item} value="item-2">
+        <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s unstyled by default, giving you freedom over the look
+          and feel.
+        </AccordionContent>
+      </Accordion.Item>
+
+      <Accordion.Item className={styles.Item} value="item-3">
+        <AccordionTrigger>Can it be animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes! You can animate the Accordion with CSS or JavaScript.
+        </AccordionContent>
+      </Accordion.Item>
+
+      {latitude && (
+        <Accordion.Item className={styles.Item} value="item-4">
+          <AccordionTrigger>Current Weather</AccordionTrigger>
+          <AccordionContent>{latitude}</AccordionContent>
+        </Accordion.Item>
+      )}
+    </Accordion.Root>
+  );
+};
 
 type AccordionTriggerProps = React.ComponentPropsWithoutRef<
   typeof Accordion.Trigger
@@ -40,20 +56,21 @@ type AccordionTriggerProps = React.ComponentPropsWithoutRef<
   className?: string;
 };
 
-const AccordionTrigger = React.forwardRef<HTMLButtonElement, AccordionTriggerProps>(
-  ({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Header className={styles.Header}>
-      <Accordion.Trigger
-        className={classNames(styles.Trigger, className)}
-        {...props}
-        ref={forwardedRef}
-      >
-        {children}
-        <ChevronDownIcon className={styles.Chevron} aria-hidden />
-      </Accordion.Trigger>
-    </Accordion.Header>
-  )
-);
+const AccordionTrigger = React.forwardRef<
+  HTMLButtonElement,
+  AccordionTriggerProps
+>(({ children, className, ...props }, forwardedRef) => (
+  <Accordion.Header className={styles.Header}>
+    <Accordion.Trigger
+      className={classNames(styles.Trigger, className)}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+      <ChevronDownIcon className={styles.Chevron} aria-hidden />
+    </Accordion.Trigger>
+  </Accordion.Header>
+));
 
 AccordionTrigger.displayName = "AccordionTrigger";
 
@@ -63,17 +80,18 @@ type AccordionContentProps = React.ComponentPropsWithoutRef<
   className?: string;
 };
 
-const AccordionContent = React.forwardRef<HTMLDivElement, AccordionContentProps>(
-  ({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Content
-      className={classNames(styles.Content, className)}
-      {...props}
-      ref={forwardedRef}
-    >
-      <div className={styles.ContentText}>{children}</div>
-    </Accordion.Content>
-  )
-);
+const AccordionContent = React.forwardRef<
+  HTMLDivElement,
+  AccordionContentProps
+>(({ children, className, ...props }, forwardedRef) => (
+  <Accordion.Content
+    className={classNames(styles.Content, className)}
+    {...props}
+    ref={forwardedRef}
+  >
+    <div className={styles.ContentText}>{children}</div>
+  </Accordion.Content>
+));
 
 AccordionContent.displayName = "AccordionContent";
 
